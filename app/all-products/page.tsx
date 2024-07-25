5fad038a7fee7bb12d3f1b0be77a007255ba4fd5
Deviant <gj8916@myamu.ac.in>
2024-07-25 14:28:17 +0530

    Trying to catch fetch error

--- app/all-products/page.tsx
+++ app/all-products/page.tsx
@@ -3,6 +3,9 @@ import { ShopPage } from "@/components/ShopPage";
 export default async function AllProductsPage() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const res = await fetch(`${baseUrl ? baseUrl : ""}/api/products`);
+  if (!res.ok) {
+    throw new Error(`Error fetching products: ${res.status}`);
+  }
   const products = await res.json();
 
   return <ShopPage products={products} />;
