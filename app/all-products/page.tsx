228de96b61ecdc58b28c3c66b68247a5ddcedc8b
Deviant <gj8916@myamu.ac.in>
2024-07-25 14:31:08 +0530

    prendering bs, fake deploy to register api with the server first

--- app/all-products/page.tsx
+++ app/all-products/page.tsx
@@ -4,7 +4,7 @@ export default async function AllProductsPage() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const res = await fetch(`${baseUrl ? baseUrl : ""}/api/products`);
   if (!res.ok) {
-    throw new Error(`Error fetching products: ${res.status}`);
+    return <div>Error fetching products</div>;
   }
   const products = await res.json();
 
